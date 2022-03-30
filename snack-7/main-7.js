18e3db270e50c19b3607785a300bd6ebab7ebbdf
FraAccaBi <francescahuda@gmail.com>
2022-03-30 16:25:43 +0200

    complete seventh snack

---
+++ snack-7/main-7.js
@@ -0,0 +1,19 @@
+/* 
+Stampa le potenze di 2 fino a 1000.
+*/
+
+let potenzeArray = [];
+
+let potenza = 0;
+for (let i = 0; potenza < 1000; i++){
+
+    potenza = Math.pow(2, i);
+    potenzeArray.push(potenza);
+    
+
+}
+
+if (potenzeArray[10] > 1000){
+        potenzeArray.pop();
+}
+console.log(potenzeArray);
