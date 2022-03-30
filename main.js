083e58bd6dbb8336fb63e0c4994d559fcceda2e1
FraAccaBi <francescahuda@gmail.com>
2022-03-30 10:46:48 +0200

    second snack

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.
 */
 
-const first_number = Number(prompt('inserisci un numero'));
+/* const first_number = Number(prompt('inserisci un numero'));
 const second_number = Number(prompt('inserisci un altro numero'));
 
 if (first_number > second_number) {
@@ -13,4 +13,21 @@ if (first_number > second_number) {
     console.log('il secondo numero è maggiore');
 } else {
     console.log('i numeri sono uguali');
+} */
+
+/* 
+SNACK 2 
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.
+ */
+
+const prima_parola = prompt('scrivi una parola');
+const seconda_parola = prompt("scrivi un'altra parola");
+
+if (prima_parola.length > seconda_parola.length){
+    console.log('la prima parola è più lunga');
+} else if (seconda_parola.length > prima_parola.length) {
+    console.log('la seconda parola è più lunga');
+} else {
+    console.log('le due parole sono lunghe uguali');
 }
