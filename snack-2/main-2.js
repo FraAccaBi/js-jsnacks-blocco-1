615c86af24d222f1091a2f2db5ee60339910a8a7
FraAccaBi <francescahuda@gmail.com>
2022-03-30 11:09:38 +0200

    add folder for each snack

--- snack-2/main-2.js
+++ snack-2/main-2.js
@@ -1,20 +1,3 @@
-/*  
-SNACK 1 
-L’utente inserisce due numeri in successione, con due prompt.
-Il software stampa il maggiore.
-*/
-
-/* const first_number = Number(prompt('inserisci un numero'));
-const second_number = Number(prompt('inserisci un altro numero'));
-
-if (first_number > second_number) {
-    console.log('il primo numero è maggiore');
-} else if (second_number > first_number) {
-    console.log('il secondo numero è maggiore');
-} else {
-    console.log('i numeri sono uguali');
-} */
-
 /* 
 SNACK 2 
 L’utente inserisce due parole in successione, con due prompt.
